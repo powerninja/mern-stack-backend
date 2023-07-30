039dec1ca94a234d85a02945aef42f49201bb8ac
powerninja <asdyuy23@gmail.com>
2023-07-30 23:31:03 +0900

    追加予定のコメント追加

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   return res.status(200).json("こんにちは");
 });
 
+//ITEM functions
+
+// USER functions
+
 app.listen(5050, () => {
   console.log("server is listening on port 5050...");
 });
