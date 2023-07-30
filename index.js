ac86989c48959fadbacf72f15c97187cc91b4029
powerninja <asdyuy23@gmail.com>
2023-07-30 23:27:16 +0900

    /パスのgetを追加

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  return res.status(200).json("こんにちは");
+});
+
 app.listen(5050, () => {
   console.log("server is listening on port 5050...");
 });
